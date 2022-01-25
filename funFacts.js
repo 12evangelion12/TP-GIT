2db8b6fd3e33651860a9c040a40fafc7208be0c7
Goulven <goulven.quere487@gmail.com>
2022-01-25 11:19:00 +0100

    FT_FunFacts: initialisation des faits cocasses

---
+++ funFacts.js
@@ -0,0 +1,14 @@
+switch(Math.floor(Math.random() * 3)){
+    case 0:
+        console.log("Firetrucks are water trucks!");
+        break;
+    case 1:
+        console.log("Whales are real!");
+        break;
+    case 3:
+        console.log("Julius Cesarius and Tifa Lockheart both got impaled in the italian senate!");
+        break;
+    case 4:
+        console.log("You share so much DNA with bananas!");
+        break;
+}
